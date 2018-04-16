2d0ee08a3c1aed8eb62434cc5de24121eae24da0
Frank Mueller <mue@tideland.biz>
2018-04-16 22:25:02 +0200

    Continued with tutorial example

--- ghpmd/ghpmd.go
+++ ghpmd/ghpmd.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/themue/ghpm/github"
+)
 
 func main() {
 	fmt.Println("GitHub Process Monitor")
+	e := github.NewRepoEventor("themue", "ghpm")
+	events, err := e.Get()
+	if err != nil {
+		fmt.Printf("error %v\n", err)
+	}
+	fmt.Printf("events %v\n", events)
 }
--- github/events.go
+++ github/events.go
@@ -1,23 +1,90 @@
 package github
 
-// RepositoryEventReader retrieves the events of one GitHub repository.
-type RepositoryEventReader struct {
-	owner         string
-	repository    string
-	authenticator Authenticator
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// EventPayload discribes the different event payload in a
+// generic way.
+type EventPayload map[string]interface{}
+
+// EventRepo describes the event repository.
+type EventRepo struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
 }
 
-// NewRepositoryEventReader creates an event reader for the given
-// owner and repository.
-func NewRepositoryEventReader(owner, repository string) *RepositoryEventReader {
-	return &RepositoryEventReader{
-		owner:      owner,
-		repository: repository,
+// EventUser describes the user behind organization and
+// event triggers.
+type EventUser struct {
+	ID         int    `json:"id"`
+	Login      string `json:"login"`
+	GravatarID string `json:"gravatar_id"`
+	AvatarURL  string `json:"avatar_url"`
+	URL        string `json:"url"`
+}
+
+// Event describes one event returned by GitHub.
+type Event struct {
+	Type      string       `json:"type"`
+	Public    bool         `json:"public"`
+	Payload   EventPayload `json:"payload"`
+	Repo      EventRepo    `json:"repo"`
+	Actor     EventUser    `json:"actor"`
+	Org       EventUser    `json:"org"`
+	CreatedAt time.Time    `json:"created_at"`
+	ID        string       `json:"id"`
+}
+
+// Events contains a number of events.
+type Events []Event
+
+// RepoEventor retrieves the events of one GitHub repository.
+type RepoEventor struct {
+	owner string
+	repo  string
+}
+
+// NewRepoEventor creates the retriever for the events of one
+// repository.
+func NewRepoEventor(o, r string) *RepoEventor {
+	return &RepoEventor{
+		owner: o,
+		repo:  r,
 	}
 }
 
-// SetAuthenticator allows the event reader to authenticate against
-// GitHub if needed.
-func (r *RepositoryEventReader) SetAuthenticator(authenticator Authenticator) {
-	r.authenticator = authenticator
+// Get retrieves the newest events for the configured repository.
+func (e *RepoEventor) Get() (Events, error) {
+	// Prepare request.
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/events", e.owner, e.repo)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	// Perform request.
+	var client http.Client
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("HTTP status code %d", resp.StatusCode)
+	}
+	// Unmarshall events.
+	var buf []byte
+	var events Events
+	_, err = resp.Body.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(buf, events)
+	if err != nil {
+		return nil, err
+	}
+	return events, nil
 }
